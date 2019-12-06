ffaad8ca950729828a44de7bab2e1753d2c3d2ec
sammy njihia <sammy.njihia@jumo.world>
2019-12-06 17:07:06 +0300

    Configure PostgreSQL database

---
+++ src/main/kotlin/com/example5/demo5/Demo5Application.kt
@@ -0,0 +1,11 @@
+package com.example5.demo5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo5Application
+
+fun main(args: Array<String>) {
+    runApplication<Demo5Application>(*args)
+}
---
+++ src/test/kotlin/com/example5/demo5/Demo5ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example5.demo5
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
