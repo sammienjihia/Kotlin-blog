e3d58dd12c2518c26921b099ddebcb7674f51492
sammy njihia <sammy.njihia@jumo.world>
2019-12-06 17:17:48 +0300

    Add the Article entity class

---
+++ README.md
@@ -0,0 +1,3 @@
+Domain models are the classes that are mapped to the corresponding tables in the database 
+
+We frequently create classes whose main purpose is to hold data. In such a class some standard functionality and utility functions are often mechanically derivable from the data. In Kotlin, this is called a data class and is marked as data:
---
+++ src/main/kotlin/com/example5/demo5/model/Article.kt
@@ -0,0 +1,19 @@
+package com.example5.demo5.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@Entity
+data class Article(
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long,
+
+        @get: NotBlank
+        val title: String = "",
+
+        @get: NotBlank
+        val content: String = ""
+)
