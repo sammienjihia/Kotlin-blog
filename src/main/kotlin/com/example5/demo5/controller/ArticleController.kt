91c02fe3abe09a3324d99832d823968574d75b01
sammy njihia <sammy.njihia@jumo.world>
2019-12-09 13:05:14 +0300

    Added the delete and put mappings

--- src/main/kotlin/com/example5/demo5/controller/ArticleController.kt
+++ src/main/kotlin/com/example5/demo5/controller/ArticleController.kt
@@ -2,6 +2,7 @@ package com.example5.demo5.controller
 
 import com.example5.demo5.model.Article
 import com.example5.demo5.repository.ArticleRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -21,6 +22,23 @@ class ArticleController(private val articleRepository: ArticleRepository){
     fun getArticleById(@PathVariable(value = "id") articleId: Long): ResponseEntity<Article>{
         return articleRepository.findById(articleId).map {
             article -> ResponseEntity.ok(article)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @PutMapping("/articles/{id}")
+    fun updateArticleById(@PathVariable(value = "id") articleId: Long,
+                          @Valid @RequestBody newArticle: Article): ResponseEntity<Article>{
+        return articleRepository.findById(articleId).map { existingArticle ->
+            val updatedArticle: Article = existingArticle.copy(title = newArticle.title, content = newArticle.content)
+            ResponseEntity.ok().body(articleRepository.save(updatedArticle))
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @DeleteMapping("/articles{id}")
+    fun deleteArticleById(@PathVariable(value = "id") articleId: Long): ResponseEntity<Void>{
+        return articleRepository.findById(articleId).map { article ->
+            articleRepository.delete(article)
+            ResponseEntity<Void>(HttpStatus.OK)
         }.orElse(ResponseEntity.notFound().build())
     }
 }
